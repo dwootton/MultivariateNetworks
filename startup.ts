83a2e595b642592be6d54367bce47a9a98e5695a
Dylan Wootton <wootton.dylan@gmail.com>
2019-05-23 07:47:51 +0200

    Migrated to typescript

--- startup.ts
+++ startup.ts
@@ -3,7 +3,7 @@ import * as d3 from 'd3';
 var margin = {top: 90, right: 0, bottom: 10, left: 90},
     width = 750,
     height = 750;
-var x = d3.scaleBand().range([0, width]),
+var x = d3.scaleBand<number>().range([0, width]),
     z = d3.scaleLinear().domain([0, 4]).clamp(true),
     c = d3.scaleOrdinal(d3.schemeCategory10);
 var svg = d3.select("body").append("svg")
@@ -12,7 +12,10 @@ var svg = d3.select("body").append("svg")
     .style("margin-right", -margin.left + "px")
   .append("g")
     .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-d3.json("data.json").then(function(data) {
+    /*
+      nodes:{index: number,count:number},
+      links:{source:{}}}*/
+d3.json("data.json").then(function(data : any ) {
   var matrix = [],
       nodes = data.nodes,
       n = nodes.length;
@@ -33,11 +36,12 @@ d3.json("data.json").then(function(data) {
     nodes[link.target].count += link.value;
   });
   // Precompute the orders.
-  var orders = {
+  var orders : any = {
     name: d3.range(n).sort(function(a, b) { return d3.ascending(nodes[a].name, nodes[b].name); }),
     count: d3.range(n).sort(function(a, b) { return nodes[b].count - nodes[a].count; }),
     group: d3.range(n).sort(function(a, b) { return nodes[b].group - nodes[a].group; })
   };
+
   // The default sort order.
   x.domain(orders.name);
   svg.append("rect")
@@ -49,7 +53,7 @@ d3.json("data.json").then(function(data) {
     .enter().append("g")
       .attr("class", "row")
       .attr("transform", function(d, i) { return "translate(0," + x(i) + ")"; })
-      .each(row);
+      .each(rowSelect);
   row.append("line")
       .attr("x2", width);
   row.append("text")
@@ -71,45 +75,50 @@ d3.json("data.json").then(function(data) {
       .attr("dy", ".32em")
       .attr("text-anchor", "start")
       .text(function(d, i) { return nodes[i].abbr; });
-  function row(row) {
+  function rowSelect(row) {
     var cell = d3.select(this).selectAll(".cell")
         .data(row.filter(function(d) { return d.z; }))
       .enter().append("rect")
         .attr("class", "cell")
-        .attr("x", function(d) { return x(d.x); })
+        .attr("x", function(d: any) { return x(d.x); })
         .attr("width", x.bandwidth())
         .attr("height", x.bandwidth())
-        .style("fill-opacity", function(d) { return z(d.z); })
-        .style("fill", function(d) { return nodes[d.x].group == nodes[d.y].group ? c(nodes[d.x].group) : null; })
+        .style("fill-opacity", function(d: any) { return z(d.z); })
+        .style("fill", function(d: any) { return nodes[d.x].group == nodes[d.y].group ? c(nodes[d.x].group) : null; })
         .on("mouseover", mouseover)
         .on("mouseout", mouseout);
   }
+
   function mouseover(p) {
     d3.selectAll(".row text").classed("active", function(d, i) { return i == p.y; });
     d3.selectAll(".column text").classed("active", function(d, i) { return i == p.x; });
   }
+
   function mouseout() {
     d3.selectAll("text").classed("active", false);
   }
+
   d3.select("#order").on("change", function() {
     clearTimeout(timeout);
-    order(this.value);
+    order("name");//d3.select("#order").node().value);
   });
+
   function order(value) {
     x.domain(orders[value]);
     var t = svg.transition().duration(2500);
     t.selectAll(".row")
-        .delay(function(d, i) { return x(i) * 4; })
+        .delay(function(d: any, i) { return x(i) * 4; })
         .attr("transform", function(d, i) { return "translate(0," + x(i) + ")"; })
       .selectAll(".cell")
-        .delay(function(d) { return x(d.x) * 4; })
-        .attr("x", function(d) { return x(d.x); });
+        .delay(function(d: any) { return x(d.x.toString()) * 4; })
+        .attr("x", function(d: any) { return x(d.x); });
     t.selectAll(".column")
-        .delay(function(d, i) { return x(i) * 4; })
+        .delay(function(d: any, i) { return x(i) * 4; })
         .attr("transform", function(d, i) { return "translate(" + x(i) + ")rotate(-90)"; });
   }
+
   var timeout = setTimeout(function() {
     order("group");
-    d3.select("#order").property("selectedIndex", 2).node().focus();
+    (<HTMLElement> d3.select("#order").property("selectedIndex", 2).node()).focus();
   }, 5000);
 });
